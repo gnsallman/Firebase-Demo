7c71cdd67599a7c6749e95b67f41c4d7e799a828
Ghulam Sallman <gnsallman@gmail.com>
2016-04-20 21:20:12 -0400

    Add button for firebase query

--- app/src/main/java/com/firebase/firebase_demo/MainActivity.java
+++ app/src/main/java/com/firebase/firebase_demo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button getDataButton = (Button) findViewById(R.id.firebase_get);
+        getDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
+
     }
 
 }
